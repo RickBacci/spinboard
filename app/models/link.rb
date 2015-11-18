f8b840040d6a592550f52d1ed406dbeecf3dc591
Rick Bacci <rickbacci@gmail.com>
2015-11-18 12:49:32 -0700

    add validations for Link model

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,16 @@
+require 'uri'
+
 class Link < ActiveRecord::Base
+  before_create :valid?
+  validates :title, presence: true
+  validates :url, presence: true
+end
+
+
+private
+
+def valid?(uri)
+  !!URI.parse(uri)
+rescue URI::InvalidURIError
+  false
 end
