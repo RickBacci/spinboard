261c1764beb331c33445d0c66fe686ac4f07b672
Rick Bacci <rickbacci@gmail.com>
2015-11-19 12:52:54 -0700

    configure sendgrid for production

--- app/mailers/recommendations_mailer.rb
+++ app/mailers/recommendations_mailer.rb
@@ -3,7 +3,6 @@ class RecommendationsMailer < ApplicationMailer
   def notify_user(user, params)
     @user = User.find(params[:user])
     @link = Link.find(params[:link])
-    @message = @link
     mail(
       to: user[:email],
       subject: "Check out my link recommendation #{@link.url}",
